7a895262e3f059bc4ea94b4862f910a6403f855d
Andrzej <acieslik.enn@gmail.com>
2022-12-11 13:20:14 +0100

    jpg

--- src/main/java/com/example/checkbox/HelloController.java
+++ src/main/java/com/example/checkbox/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.checkbox;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private CheckBox myCheckBox;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label myLabel;
+    @FXML
+    private ImageView myImageView;
+
+    Image myImage1 = new Image(getClass().getResourceAsStream("foto1.jpg"));
+    Image myImage2 = new Image(getClass().getResourceAsStream("foto10.jpg"));
+
+    public void change(ActionEvent event){
+        if(myCheckBox.isSelected()){
+            myLabel.setText("ON");
+            myImageView.setImage(myImage1);
+        } else {
+            myLabel.setText("OFF");
+            myImageView.setImage(myImage2);
+        }
     }
 }
